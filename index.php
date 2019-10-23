dde5e71db24fad7f333072b4d4e4cd37858b7757
Jcrespo <jcrespo@wikimedia.org>
2019-10-23 10:51:52 +0000

    Revert "mysql: Migrate away from long-deprecated mysql module to mysqli"
    This reverts commit ac66281186db09a32e0252e4d4ea67af0f9f7c43.
    
    Reason for revert: Reguired more refactoring, unbreaking HEAD
    
    Change-Id: Id1c142142e54648d9d778142585979436b7e4fa7

--- index.php
+++ index.php
@@ -28,9 +28,9 @@ function db()
     if (is_null($db) || !is_resource($db))
     {
         e("db connect: host");
-        $db = @mysqli_connect($db_host, $db_user, $db_pass)
+        $db = @mysql_connect($db_host, $db_user, $db_pass)
             or db_fail($db_name, $db_host);
-        mysqli_select_db($db_name);
+        mysql_select_db($db_name);
     }
     return $db;
 }
